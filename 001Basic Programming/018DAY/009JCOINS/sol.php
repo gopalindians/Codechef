952de16298063418f72a5dc281eaf4af3feebed4
Gopal Sharma <gopalsharma@Gopals-iMac.local>
2022-11-11 12:59:24 +0530

    018DAY:Afternoon session

--- 001Basic Programming/018DAY/009JCOINS/sol.php
+++ 001Basic Programming/018DAY/009JCOINS/sol.php
@@ -5,7 +5,7 @@ fscanf(STDIN, "%d", $t);
 if( $t>=1 && $t<=100)
     $c = range(1,$t);
     foreach($c as $k){
-        fscanf(STDIN, "%d", $x);
-        if($x>=1 && $x<=100)
-            echo (100-$x).PHP_EOL;
+        fscanf(STDIN, "%d %d", $x, $y);
+        if($x>=0 && $x<=100 && $y>=0 && $y<=100)
+            echo ($x*10+$y*5).PHP_EOL;
     }
